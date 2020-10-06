63d59b3cde4a2b93b9869a8faa28f1f46903b4ac
Muhammad-Suleman-Ali <70519146+Muhammad-Suleman-Ali@users.noreply.github.com>
2020-10-06 16:26:14 +0500

    updating our App with SignIn page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Route,Switch} from 'react-router-dom';
 import Shoppage from './pages/Homepage/shop/shop.component.js';
 import './App.css';
 import Header from '../src/components/header/header.component';
+import SignInAndSignUpPage from './pages/Homepage/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
   <Route exact path='/' component={Homepage}/> 
  
   <Route exact path='/shop' component={Shoppage}/> 
+  <Route exact path='/signin' component={SignInAndSignUpPage}/>
   </Switch>
     
    </div>
